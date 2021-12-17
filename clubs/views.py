ac016a4d197218453d44f96f674d315ab750008d
barnes1999 <richard.rixpix.barnes@gmail.com>
2021-12-17 10:44:07 +0000

    Base version of log in

--- clubs/views.py
+++ clubs/views.py
@@ -4,6 +4,9 @@ from .forms import SignUpForm
 def home (request):
     return render(request,'home.html')
 
+def log_in(request):
+    return render(request,'log_in.html')
+
 def sign_up(request):
     form = SignUpForm()
     return render(request,'sign_up.html',{'form':form})
